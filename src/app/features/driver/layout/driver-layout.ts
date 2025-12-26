2f03e0fa7ebac8b4ef51e3cc3df4f7d0f6d15c70
farjad122334 <muhammadfarjad102@gmail.com>
2025-12-26 20:27:43 +0500

    changed driver dashboard layout

--- src/app/features/driver/layout/driver-layout.ts
+++ src/app/features/driver/layout/driver-layout.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 @Component({
     standalone: true,
     selector: 'app-driver-layout',
-    imports: [RouterModule],
+    imports: [CommonModule, RouterModule],
     templateUrl: './driver-layout.html',
     styleUrl: './driver-layout.css'
 })
 export class DriverLayout {
+    isSidebarOpen = false;
 
+    toggleSidebar() {
+        this.isSidebarOpen = !this.isSidebarOpen;
+    }
 }
--- src/app/features/tourist/layout/tourist-layout.ts
+++ src/app/features/tourist/layout/tourist-layout.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 @Component({
     standalone: true,
     selector: 'app-tourist-layout',
-    imports: [RouterModule],
+    imports: [RouterModule, CommonModule],
     templateUrl: './tourist-layout.html',
     styleUrl: './tourist-layout.css'
 })
 export class TouristLayout {
+    isSidebarOpen = false;
 
+    toggleSidebar() {
+        this.isSidebarOpen = !this.isSidebarOpen;
+    }
 }
